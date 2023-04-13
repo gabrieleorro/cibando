bed12b627b63255056cb29d067354e511a941ece
gabrieleorro <53332032+gabrieleorro@users.noreply.github.com>
2023-04-13 11:51:17 +0200

    Esternato modulo ricette, prima di esternare modulo user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,19 +10,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { DividerModule } from 'primeng/divider';
-import { PaginatorModule } from 'primeng/paginator';
 import { ToastModule } from 'primeng/toast';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { RecipesModule } from './components/recipes/recipes.module';
+
 import { CarouselComponent } from './components/carousel/carousel.component';
-import { RecipesComponent } from './components/recipes/recipes.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { RecipeCardComponent } from './shared/recipe-card/recipe-card.component';
-import { DetailComponent } from './components/recipes/detail/detail.component';
-import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
-import { NewRecipeComponent } from './components/recipes/new-recipe/new-recipe.component';
 import { ChangeColorDirective } from './directives/change-color.directive';
 import { EsempioCombineComponent } from './components/esempio-combine/esempio-combine.component';
 import { LoginComponent } from './components/user/login/login.component';
@@ -33,15 +28,10 @@ import { ProfileComponent } from './components/user/profile/profile.component';
   declarations: [
     AppComponent,
     CarouselComponent,
-    RecipesComponent,
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    RecipeCardComponent,
-    DetailComponent,
-    RecipesListComponent,
     RegistrationComponent,
-    NewRecipeComponent,
     ChangeColorDirective,
     EsempioCombineComponent,
     LoginComponent,
@@ -58,10 +48,10 @@ import { ProfileComponent } from './components/user/profile/profile.component';
     ReactiveFormsModule,
     PasswordModule,
     DividerModule,
-    PaginatorModule,
     HttpClientModule,
     ToastModule,
     CKEditorModule,
+    RecipesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/recipes/recipes.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PaginatorModule } from 'primeng/paginator';
+import { ToastModule } from 'primeng/toast';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipesComponent } from './recipes.component';
+import { DetailComponent } from './detail/detail.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { RecipeCardComponent } from 'src/app/shared/recipe-card/recipe-card.component';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeCardComponent,
+    RecipesListComponent,
+    DetailComponent,
+    NewRecipeComponent,
+  ],
+  imports: [
+    CommonModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PaginatorModule,
+    ToastModule,
+    CKEditorModule,
+    RecipesRoutingModule,
+    HttpClientModule,
+  ],
+  exports: [RecipeCardComponent]
+})
+
+export class RecipesModule {}
